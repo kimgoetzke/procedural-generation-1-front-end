68009dfd16b860b182e4c18bac7abd964319f254
Kim Goetzke <kbgoetzke@gmail.com>
2023-12-12 18:58:31 +0000

    Replace useEffect with callback on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,45 +1,42 @@
 "use client";
 
-import React, {useEffect, useState} from "react";
+import React from "react";
 import {useRouter} from "next/navigation";
 import {Button} from "@/components/ui/button";
 
 export default function Home() {
-    const [shouldFetch, setShouldFetch] = useState(false);
     const router = useRouter();
 
-    useEffect(() => {
-        if (shouldFetch) {
-            fetch(`/api/play`, {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json'
-                }
+    const onClickPlay = () => {
+        fetch(`/api/play`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(res => res.json())
+            .then(data => {
+                localStorage.setItem("webResponse", JSON.stringify(data));
+                router.replace('/play');
             })
-                .then(res => res.json())
-                .then(data => {
-                    localStorage.setItem("webResponse", JSON.stringify(data));
-                    setShouldFetch(false);
-                    router.replace('/play');
-                })
-        }
-    }, [shouldFetch]);
+    }
 
     return (
         <main className="flex min-h-screen items-center justify-center">
             <div className="flex flex-col items-center justify-center">
-                <h1 className="standard-h1 justify-center p-2">Welcome to King of Castrop Rauxel!</h1>
-                <p className="pb-8 pt-4 text-muted-foreground">A game of skill and chance.</p>
-                <Button onClick={() => handleClick(setShouldFetch)}
-                        className="standard-button text-secondary standard-bg-gradient uppercase p-6 font-bold text-3xl">Play
+                <h1 className="standard-h1 justify-center p-2">
+                    Welcome to
+                    <span className="underline underline-offset-4">King of Castrop Rauxel</span>
+                    !
+                </h1>
+                <p className="pt-4 text-muted-foreground">The web implementation of a text-based adventure.</p>
+                <p className="pb-8 text-muted-foreground"> Explore a huge, procedurally generated world, complete
+                    quests, and engage in combat to get stronger and gain loot.</p>
+                <Button onClick={() => onClickPlay()}
+                        className="standard-button text-secondary standard-bg-gradient uppercase p-6 font-bold text-3xl">
+                    Play
                 </Button>
             </div>
         </main>
     )
-}
-
-const handleClick = (
-    setShouldFetch: (value: (((prevState: boolean) => boolean) | boolean)) => void
-) => {
-    setShouldFetch(true);
 }
