d120a5ba9df6d1cd58219a0f3b2ba2068e53c6fa
Kim Goetzke <kbgoetzke@gmail.com>
2024-01-01 17:34:59 +0000

    Center home page text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,14 +32,16 @@ export default function Home() {
 
     return (
         <main className="flex min-h-screen items-center justify-center">
-            <div className="flex flex-col items-center justify-center">
+            <div className="flex flex-col items-center m-3">
                 <h1 className="standard-h1 justify-center p-2">
                     Welcome to <span className="underline underline-offset-4">
                     King of Castrop Rauxel</span>!
                 </h1>
-                <p className="pt-4 text-muted-foreground">The web interface for the text-based adventure game.</p>
-                <p className="pb-8 text-muted-foreground"> Explore a huge, procedurally generated world, complete
-                    quests, and engage in combat to get stronger and gain loot.</p>
+                <p className="w-full pt-4 pb-2 px-2 text-muted-foreground text-left sm:text-center">
+                    The web interface for the text-based adventure game.</p>
+                <p className="w-full pb-8 px-2 text-muted-foreground text-left sm:text-center">
+                    Explore a huge, procedurally generated world, complete quests, and engage in combat to get stronger
+                    and gain loot.</p>
                 <Button onClick={() => onClickPlay()}
                         className="standard-button text-secondary standard-bg-gradient uppercase p-6 font-bold text-3xl">
                     Play
